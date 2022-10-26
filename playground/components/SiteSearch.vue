a55314b6ae54c5de655038907bcce63bacb8411e
Silvano Stralla <silvano.stralla@sistrall.it>
2022-10-26 15:00:54 +0200

    Improve playground code

--- playground/components/SiteSearch.vue
+++ playground/components/SiteSearch.vue
@@ -9,7 +9,7 @@ const { state, error, data } = useSiteSearch({
   // optional: you can omit it you only have one locale, or you want to find results in every locale
   initialState: { locale: 'en' },
   // optional: defaults to 8 search results per page
-  resultsPerPage: 10,
+  resultsPerPage: 10
 })
 
 </script>
@@ -17,27 +17,29 @@ const { state, error, data } = useSiteSearch({
 <template>
   <div class="container mx-auto">
     <div class="py-8">
-      <input class="w-full p-4 border border-solid border-gray-300 rounded" type="text" v-model="state.query" placeholder="Search... " />
+      <input v-model="state.query" class="w-full p-4 border border-solid border-gray-300 rounded" type="text" placeholder="Search... ">
     </div>
-    <hr />
-    <div class="py-8" v-if="data">
-      <div v-for="result in data.pageResults" class="py-4">
+    <hr>
+    <div v-if="data" class="py-8">
+      <div v-for="result in data.pageResults" :key="result.id" class="py-4">
         <p class="py-1">
           <strong v-if="result.titleHighlights.length > 0">
-            <template v-for="highlight in result.titleHighlights" class="py-1">
-              <template v-for="piece in highlight">
+            <span v-for="(highlight, index) in result.titleHighlights" :key="index">
+              <span v-for="piece in highlight" :key="piece.text">
                 <mark v-if="piece.isMatch">{{ piece.text }}</mark>
-                <template v-else>{{ piece.text }}</template>
-              </template>
-            </template>
+                <span v-else :key="piece.text">{{ piece.text }}</span>
+              </span>
+            </span>
           </strong>
           <strong v-else>{{ result.title }}</strong>
         </p>
-        <p v-for="highlight in result.bodyHighlights" class="py-1">
-          <template v-for="piece in highlight">
+        <p v-for="(highlight, index) in result.bodyHighlights" :key="index" class="py-1">
+          <span v-for="piece in highlight" :key="piece.text">
             <mark v-if="piece.isMatch">{{ piece.text }}</mark>
-            <template v-else>{{ piece.text }}</template>
-          </template>
+            <span v-else>
+              {{ piece.text }}
+            </span>
+          </span>
         </p>
         <div>
           <pre><code class="block whitespace-pre overflow-x-scroll">{{ JSON.stringify(result.raw, null, 2) }}</code></pre>
